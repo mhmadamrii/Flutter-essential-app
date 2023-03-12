17a1018c3283a58067932bca49c2065be0905679
mhmadamrii <interceptorghost4@gmail.com>
2023-03-12 15:33:11 +0700

    mwehehehe

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:basic/chat-gpt/save_us.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -11,42 +12,81 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const RootPage(),
+      home: const SaveUs(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-// class RootPage extends StatelessWidget {
-//   const RootPage({super.key});
+class Apollo extends StatefulWidget {
+  const Apollo({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text("Hello world"),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: () {},
-//         child: const Icon(
-//           Icons.add,
-//         ),
-//       ),
-//       bottomNavigationBar: NavigationBar(
-//         destinations: const [
-//           NavigationDestination(
-//             icon: Icon(Icons.home),
-//             label: 'Home',
-//           ),
-//           NavigationDestination(
-//             icon: Icon(Icons.person),
-//             label: 'Profile',
-//           ),
-//         ],
-//         onDestinationSelected: (int index) {},
-//       ),
-//     );
-//   }
-// }
+  @override
+  State<Apollo> createState() => _ApolloState();
+}
+
+class _ApolloState extends State<Apollo> {
+  int number = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text(
+          "wowkwk",
+        ),
+      ),
+      body: Column(
+        mainAxisAlignment: number == 0
+            ? MainAxisAlignment.start
+            : number == 1
+                ? MainAxisAlignment.center
+                : MainAxisAlignment.end,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment:
+                number == 0 ? MainAxisAlignment.start : MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ElevatedButton(
+                onPressed: (() {
+                  setState(() {
+                    number++;
+                    print(number);
+                  });
+                }),
+                child: const Text(
+                  "Press",
+                ),
+              ),
+              OutlinedButton(
+                onPressed: () {
+                  setState(() {
+                    number = 0;
+                    print(number);
+                  });
+                },
+                child: const Text(
+                  "Reset number",
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.green,
+        child: IconButton(
+          onPressed: (() {}),
+          icon: const Icon(
+            Icons.home,
+          ),
+        ),
+      ),
+    );
+  }
+}
 
 class RootPage extends StatefulWidget {
   const RootPage({super.key});
