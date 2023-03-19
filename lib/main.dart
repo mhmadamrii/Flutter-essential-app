3db19ee1ec5f9714e615421bda7331cfdf976219
mhmadamrii <interceptorghost4@gmail.com>
2023-03-19 10:53:20 +0700

    againnn

--- lib/main.dart
+++ lib/main.dart
@@ -7,33 +7,157 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Whatever(),
+    return MaterialApp(
+      home: const MyHomePage(),
+      theme: ThemeData(
+        primarySwatch: Colors.brown,
+      ),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-class Whatever extends StatefulWidget {
-  const Whatever({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
 
   @override
-  State<Whatever> createState() => _WhateverState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _WhateverState extends State<Whatever> {
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  final List<Widget> _pagess = const [HomeScreen(), SearchPage()];
+
+  void _onItemTapped(int index) {
+    // ignore: avoid_print
+    print("the index is $index");
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Hello world"),
+        title: const Text("Flutter app"),
+        actions: [
+          IconButton(
+            onPressed: (() {}),
+            icon: const Icon(Icons.set_meal),
+          ),
+        ],
       ),
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () {},
-        child: const Text(
-          "Here we go",
+      body: _pagess[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: "Search",
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          border: Border.all(width: 1),
         ),
-      )),
+        width: double.infinity,
+        child: const Center(
+          child: Text(
+            "Hello world, this is screen page",
+            style: TextStyle(
+              fontSize: 40,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  String _userInput = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: TextField(
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "Enter your word",
+                ),
+                onChanged: (value) {
+                  setState(() {
+                    _userInput = value;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30),
+              child: Text(
+                _userInput,
+                style: const TextStyle(
+                  fontSize: 50,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() {
+          showModalBottomSheet(
+            context: context,
+            builder: (BuildContext context) {
+              return const MyModalBottomSheet();
+            },
+          );
+        }),
+        child: const Icon(Icons.phone_android),
+      ),
+    );
+  }
+}
+
+class MyModalBottomSheet extends StatelessWidget {
+  const MyModalBottomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // ignore: avoid_unnecessary_containers
+    return Container(
+      child: const Center(
+        child: Text("This is new modal bottom"),
+      ),
     );
   }
 }
