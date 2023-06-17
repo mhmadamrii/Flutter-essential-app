dc0999af93df4933e04fced6363c979dfa19a36a
mhmadamrii <interceptorghost4@gmail.com>
2023-06-17 11:48:13 +0700

    add some fucking stupid

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,67 @@
-import 'package:basic/screens/home.dart';
-import 'package:basic/screens/instagram.dart';
 import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/instagram',
+      home: Homepage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/instagram': (context) => const Instagram(),
-      },
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final Dio dio = Dio();
+
+  String _datas = "";
+  Future<void> fetchData() async {
+    try {
+      Response res =
+          await dio.get("https://jsonplaceholder.typicode.com/posts");
+      setState(() {
+        _datas = res.data.toString();
+      });
+      print(res.data);
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ElevatedButton(
+                onPressed: fetchData,
+                child: const Text(
+                  "Press to fetch",
+                ),
+              ),
+              Text(
+                _datas,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
