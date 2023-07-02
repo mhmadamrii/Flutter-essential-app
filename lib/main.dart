3308d0d37ea98f2a3c2053106a6258a6dd125df2
mhmadamrii <interceptorghost4@gmail.com>
2023-07-02 20:23:25 +0700

    add some shitty ...

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,72 @@
+import 'package:basic/screens/another.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Homepage(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Another(),
     );
   }
 }
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+  Homepage({super.key});
+// primitive data type
+  final String name = "John";
+  final int age = 40;
+  final bool isLogin = true;
 
+  String inputValue = '';
+
+// nonprimitive data type
+  final List<String> names = ["John", "Doe", "Foo", "Bar"];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              Text("The name is: $name"),
+              Text("The age is: $age"),
+              if (isLogin) ...[
+                const Text("Hello user"),
+              ] else ...[
+                const Text("No user found")
+              ],
+              Expanded(
+                child: ListView.builder(
+                  itemCount: names.length,
+                  itemBuilder: (context, index) {
+                    return Text(names[index]);
+                  },
+                ),
+              ),
+              TextField(
+                onChanged: (value) => inputValue = value,
+                decoration: const InputDecoration(
+                  labelText: "Input here..",
+                  border: OutlineInputBorder(),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => print("input value: $inputValue"),
+        child: const Icon(
+          Icons.add,
+        ),
+      ),
+    );
   }
 }
