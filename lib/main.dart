9d9d0f3ea26c1b3adcc51ced26d4ca6269856c99
mhmadamrii <interceptorghost4@gmail.com>
2023-07-09 19:47:21 +0700

    add several clues

--- lib/main.dart
+++ lib/main.dart
@@ -4,65 +4,118 @@ import 'package:flutter/material.dart';
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Another(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const Another(),
     );
   }
 }
 
-class Homepage extends StatelessWidget {
-  Homepage({super.key});
-// primitive data type
-  final String name = "John";
-  final int age = 40;
-  final bool isLogin = true;
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
 
-  String inputValue = '';
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
 
-// nonprimitive data type
-  final List<String> names = ["John", "Doe", "Foo", "Bar"];
+class _HomepageState extends State<Homepage> {
+  // Initialize lists of todos
+  List<String> todos = [];
+
+  void addTodo() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        TextEditingController controller = TextEditingController();
+        return AlertDialog(
+          title: const Text(
+            "Add todo",
+          ),
+          content: TextField(
+            controller: controller,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+          ),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  todos.add(controller.text);
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text("Add"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void deleteTodo(int indexTodo) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Delete Todo"),
+          content: const Text(
+            "Are you sure to delete todo?",
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Cancel"),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                setState(() {
+                  todos.removeAt(indexTodo);
+                });
+                Navigator.of(context).pop();
+              },
+              child: const Text("Delete"),
+            )
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text("Todo application"),
+        elevation: 0,
+      ),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            children: [
-              Text("The name is: $name"),
-              Text("The age is: $age"),
-              if (isLogin) ...[
-                const Text("Hello user"),
-              ] else ...[
-                const Text("No user found")
-              ],
-              Expanded(
-                child: ListView.builder(
-                  itemCount: names.length,
-                  itemBuilder: (context, index) {
-                    return Text(names[index]);
-                  },
+        child: ListView.builder(
+          itemCount: todos.length,
+          itemBuilder: (BuildContext context, index) {
+            return ListTile(
+              title: Text(todos[index]),
+              trailing: GestureDetector(
+                onTap: () {
+                  deleteTodo(index);
+                },
+                child: const Icon(
+                  Icons.delete,
+                  color: Colors.red,
                 ),
               ),
-              TextField(
-                onChanged: (value) => inputValue = value,
-                decoration: const InputDecoration(
-                  labelText: "Input here..",
-                  border: OutlineInputBorder(),
-                ),
-              )
-            ],
-          ),
+            );
+          },
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => print("input value: $inputValue"),
+        onPressed: addTodo,
         child: const Icon(
           Icons.add,
         ),
--- lib/screens/another.dart
+++ lib/screens/another.dart
@@ -1,40 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Another extends StatefulWidget {
-  const Another({Key? key}) : super(key: key);
+  const Another({super.key});
 
   @override
   State<Another> createState() => _AnotherState();
 }
 
 class _AnotherState extends State<Another> {
-  TextEditingController _controller = TextEditingController();
-  String currentValue = '';
-  @override
-  void initState() {
-    super.initState();
-    currentValue = _controller.text;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return const Scaffold(
+      body: SafeArea(
         child: Column(
-          children: [
-            TextField(
-              controller: _controller,
-              decoration: const InputDecoration(
-                border: OutlineInputBorder(),
-              ),
-            ),
-            Text(
-              currentValue,
-              style: TextStyle(
-                fontSize: 30,
-              ),
-            ),
-          ],
+          children: [],
         ),
       ),
     );
